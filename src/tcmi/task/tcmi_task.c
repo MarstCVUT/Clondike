a713fe34e4dadc1cd9b21b10b3a1d3e3b0e9ecac
user <martin@stava.me>
2010-04-26 20:46:24 +0200

    Fix: Properly initialize reference to remote pid file in constructor.

--- src/tcmi/task/tcmi_task.c
+++ src/tcmi/task/tcmi_task.c
@@ -124,6 +124,7 @@ int tcmi_task_init(struct tcmi_task *self, pid_t local_pid, struct tcmi_migman* 
 	self->execve_count = 0;
 
 	self->d_migman_root = d_migman;
+	self->f_remote_pid_rev = NULL;
 	tcmi_queue_init(&self->method_queue);
 	tcmi_queue_init(&self->msg_queue);
 
