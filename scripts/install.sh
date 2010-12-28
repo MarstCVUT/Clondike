39403f4e980868b8d9a833825c55ce56899984fa
Petr Malat <oss@malat.biz>
2010-12-28 12:26:56 +0100

    Added install.sh

---
+++ scripts/install.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+###################################################################
+#                       install.sh                                #
+#                                                                 #
+# Description: installation steps include:                        #
+#              - creating new user 'clondike' w/ uid=999          #
+#              - creating ctlfs mount point at /clondike          #
+#                                                                 #
+#                                                                 #
+# Synopsis:                                                       #
+# install.sh                                                      #
+#                                                                 #
+# Date: 05/15/2005                                                #
+#                                                                 #
+# Author: Jan Capek                                               #
+#                                                                 #
+# $Id: install.sh,v 1.2 2008/05/02 19:59:20 stavam2 Exp $
+#                                                                 #
+###################################################################
+
+
+CLONDIKEUSER=clondike
+CLONDIKEUID=999
+CLONDIKEDIR=/clondike
+CLONDIKEMNT=/mnt/clondike
+CLONDIKEHOME=/home/clondike
+PROXYDIR=/mnt/proxy
+
+adduser --uid $CLONDIKEUID $CLONDIKEUSER  && {
+    echo "Created new user '$CLONDIKEUSER', uid=$CLONDIKEUID."
+}
+
+[ ! -d $CLONDIKEDIR ] && {
+    mkdir $CLONDIKEDIR
+    echo "Created ctlfs directory '$CLONDIKEDIR'."
+}
+
+[ ! -d $CLONDIKEMNT ] && {
+    mkdir $CLONDIKEMNT
+    echo "Created mounting directory '$CLONDIKEMNT'."
+}
+
+[ ! -d $CLONDIKEHOME ] && {
+    mkdir $CLONDIKEHOME
+    echo "Created home directory '$CLONDIKEHOME'."
+}
+
+[ ! -d $PROXYDIR ] && {
+    mkdir $PROXYDIR
+    echo "Created ctlfs directory '$PROXYDIR'."
+}
+
