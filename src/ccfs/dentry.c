1c295c8384503a84b9844cd7cb2a39e9a282215e
martin <martin@fit-q4.(none)>
2011-06-11 19:15:04 +0200

    Fix revalidation of dentries

--- src/ccfs/dentry.c
+++ src/ccfs/dentry.c
@@ -40,7 +40,7 @@ static int ccfs_d_revalidate(struct dentry *dentry, struct nameidata *nd)
 		fsstack_copy_attr_all(dentry->d_inode, lower_inode);
 	}
 out:
-        if ( ccfs_inode_to_private(dentry->d_inode)->cacheable ) {
+        if ( !ccfs_inode_to_private(dentry->d_inode)->cacheable ) {
 	    mdbg(INFO3,"Non-cacheable inode -> invalid");
 	    return 0;
 	}
