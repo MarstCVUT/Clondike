76208d966f576d0290f759a3f51b637363c13ea8
martin <martin@fit-q4.(none)>
2011-06-11 10:35:23 +0200

    Fix compilation problem with ccfs filter

--- src/ccfs/filter.c
+++ src/ccfs/filter.c
@@ -96,7 +96,7 @@ static int pattern_matches(struct filter_pattern* filter_pattern, const char* na
 
 	if ( filter_pattern->pattern == NULL ) {
 	  /* Match time pattern. Do not check age for dir, they can be always cached since readdir is uncached anyway */
-	  mdbg(INFO3, "Date compare: %d vs %d", &filter_pattern->older_than.tv_spec, ctime->tv_spec);
+	  mdbg(INFO3, "Date compare: %d vs %d", &filter_pattern->older_than.tv_sec, ctime->tv_sec);
 	  return S_ISDIR(umode) || timespec_compare(&filter_pattern->older_than, ctime) > 0;
 	} else {		  	
 	  /* Match string pattern */
