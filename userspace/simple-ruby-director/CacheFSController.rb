601486a35b6a271bd8c2978c7c5ffe685214414a
martin <martin@fit-q4.(none)>
2011-06-12 15:31:41 +0200

    Additional specific path mounted by ccfs (used in compilation tests)

--- userspace/simple-ruby-director/CacheFSController.rb
+++ userspace/simple-ruby-director/CacheFSController.rb
@@ -43,9 +43,11 @@ private
       
     # TODO: This is hardcoded for current measurement, but should be generalized. It tries to mount a specified path 
     # so that only files older than current time are cached, the others remain uncached. Useful for kernel compilation testing
-    restrictedMountPath = "#{prefix}/usr/src/linux-2.6.33.1"
-    system("umount #{restrictedMountPath}")
-    system("mount -t ccfs -o cache_filter=OLD #{restrictedMountPath} #{restrictedMountPath}")      
+    restrictedMount = ["/usr/src/linux-2.6.33.1", "/root/clondike"]
+    restrictedMount.each { |path|
+      system("umount #{prefix + path}")
+      system("mount -t ccfs -o cache_filter=OLD #{prefix + path} #{prefix + path}")      
+    }
   end
     
 end
