e9537bb58a760d3e008d4649e5500d5231e5bfa6
martin <martin@fit-q4.(none)>
2011-06-25 11:07:03 +0200

    Exec privileges + removed debug from clear-checkpoints script

--- scripts/clear-checkpoints.rb
+++ scripts/clear-checkpoints.rb
@@ -2,11 +2,12 @@
 
 require 'fileutils'
 
+# This script is for cases when rm -rf does not work.. it fails, when too many checkpoints accumlate - it crashes with too many arguments error
+
 Dir.foreach("/home/clondike/") do |f|
   fullName = "/home/clondike/#{f}"  
 
   next if f =~ /^\..?/ # Exclude . and ..
-  next if File.directory?(fullName)
-  puts "DELETING #{fullName}"
+  next if File.directory?(fullName)  
   FileUtils.rm(fullName)
 end
